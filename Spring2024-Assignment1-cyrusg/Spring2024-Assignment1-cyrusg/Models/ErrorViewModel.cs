2ee06734a8f4ae10478472ffddf3e34bcc6e1f7e
cyrusgomeswork <chgomes@crimson.ua.edu>
2024-09-23 14:41:01 -0500

    assignment 1

---
+++ Spring2024-Assignment1-cyrusg/Spring2024-Assignment1-cyrusg/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2024_Assignment1_cyrusg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Spring2024-Assignment1-cyrusg/Spring2024-Assignment1-cyrusg/wwwroot/script.js
@@ -0,0 +1,2 @@
+﻿// Placeholder script, add JS functionality here if needed
+console.log('Interstellar fan page loaded successfully!');
